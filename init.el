0db34d66f82deb41ec86f21e424f8dbd26a0bb0b
Ben Bleything <ben@bleything.net>
2011-02-01 22:43:23 -0800

    add empty init.el

---
+++ init.el
@@ -0,0 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Ben Bleything's emacs setup
+;;; Based on the work of many others. See README.md for credits.
+;;;
+;;; Git Hubs: http://github.com/bleything/dotemacs
+;;; Internet Electronic Mail: ben@bleything.net
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; To install, make this directory live at ~/.emacs.d
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
