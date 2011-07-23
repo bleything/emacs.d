16b6c3dd2226f605ccae88f633728c481f56dbe5
Ben Bleything <ben@bleything.net>
2011-07-23 16:02:28 -0700

    slime setup

--- languages.el
+++ languages.el
@@ -13,7 +13,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; configure slime to use clisp
-(setq inferior-lisp-program "/usr/bin/env clisp")
+(add-to-list 'load-path (concat vendor-dir "slime/"))
+;(setq inferior-lisp-program "/usr/bin/env clisp")
+(setq inferior-lisp-program "clisp")
+(require 'slime)
+(slime-setup)
 
 ;;; enable paredit for (e)lisp
 (autoload 'paredit-mode "paredit"
@@ -22,3 +26,12 @@
 
 (add-hook 'lisp-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
+
+;;; disable automatic newlines in paredit
+;; (eval-after-load 'paredit
+;;   '(progn
+;;      (define-key paredit-mode-map (kbd ")")
+;;        'paredit-close-parenthesis)
+;;     
+;;      (define-key paredit-mode-map (kbd "M-)")
+;;        'paredit-close-parenthesis-and-newline)))
