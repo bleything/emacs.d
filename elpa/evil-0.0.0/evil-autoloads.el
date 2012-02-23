807a47a8d79b2315cdf2ca48c23f5e9646ab986a
Ben Bleything <ben@bleything.net>
2012-02-22 16:34:39 -0800

    drop evil (and undo-tree)

--- elpa/evil-0.0.0/evil-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; evil-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "evil-core" "evil-core.el" (20287 64193))
-;;; Generated autoloads from evil-core.el
- (autoload 'evil-mode "evil")
-
-;;;***
-
-;;;### (autoloads nil nil ("evil-commands.el" "evil-common.el" "evil-digraphs.el"
-;;;;;;  "evil-ex.el" "evil-integration.el" "evil-macros.el" "evil-maps.el"
-;;;;;;  "evil-pkg.el" "evil-repeat.el" "evil-search.el" "evil-states.el"
-;;;;;;  "evil-types.el" "evil-vars.el" "evil.el") (20287 64193 824008))
-
-;;;***
-
-(provide 'evil-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-autoloads.el ends here
