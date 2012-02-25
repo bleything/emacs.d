aef4adea048983d229678a8a3b40313004339037
Ben Bleything <ben@bleything.net>
2012-02-25 15:14:33 -0800

    fix gnu elpa location

--- init.el
+++ init.el
@@ -41,7 +41,7 @@
 
 (require 'package)
 (setq package-archives '(
-                         ("gnu"       . "http://elpa.gnu.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("tromey"    . "http://tromey.com/elpa/")))
 (package-initialize)
