1a5a51579ba61f3027e60505535c59c4aa3a541e
Ben Bleything <ben@bleything.net>
2012-03-01 22:24:27 -0800

    clean up languages.el

--- languages.el
+++ languages.el
@@ -12,22 +12,19 @@
 ;;; CSS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-hook 'css-mode-hook (lambda () (rainbow-mode)))
+(add-hook 'css-mode-hook 'rainbow-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Lisp
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; enable paredit for (e)lisp
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code."
-  t)
-
+(autoload 'paredit-mode "paredit")
 (add-hook 'lisp-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
 
 ;;; enable rainbow-delimiters for (e)lisp
-(autoload 'rainbow-delimiters-mode "rainbow-delimiters" t)
+(autoload 'rainbow-delimiters-mode "rainbow-delimiters")
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
@@ -44,34 +41,27 @@
 ;;; Markdown
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-
-(setq auto-mode-alist
-  (cons '("\\.md" . markdown-mode) auto-mode-alist))
-
-(setq auto-mode-alist
-  (cons '("\\.mkd" . markdown-mode) auto-mode-alist))
+(autoload 'markdown-mode "markdown-mode")
+(add-to-list 'auto-mode-alist '("\\.mk?d$" . markdown-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; prolog
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq prolog-system 'swi)
-(setq auto-mode-alist (append '(("\\.plg$" . prolog-mode)
-                                ("\\.m$" . mercury-mode))
-                              auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.plg$" . prolog-mode))
+(add-to-list 'auto-mode-alist '("\\.m$" . mercury-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; ruby
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'auto-mode-alist '("\\.rake" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '(".irbrc" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.irbrc$" . ruby-mode))
 
-;; disable inserting encoding comments in utf-8 files
+;;; disable inserting encoding comments in utf-8 files
 (setq ruby-insert-encoding-magic-comment nil)
 
 ;;; enable rvm support
@@ -82,5 +72,4 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (autoload 'yaml-mode "yaml-mode")
-(add-to-list 'auto-mode-alist '(".yml" . yaml-mode))
-(add-to-list 'auto-mode-alist '(".yaml" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
