351f1fe40b0704296ec61989b1607095c38d33b5
Ben Bleything <ben@bleything.net>
2012-03-02 07:21:25 -0800

    add comment-or-uncomment-region-or-line, bound to cmd-/

--- functions.el
+++ functions.el
@@ -17,3 +17,26 @@
   (insert "\n")
   (insert-char ?\; 80)
   (insert "\n"))
+
+;;; textmate-style comment/uncomment cmd-/ for toggling comments
+(global-set-key (kbd "s-/") 'comment-or-uncomment-region-or-line)
+(defun comment-or-uncomment-region-or-line () (interactive)
+  ;; redefining deactivate-mark prevents the region from disappearing
+  ;; after commenting
+  (let ((deactivate-mark nil))
+    (if mark-active
+        ;; save-excursion resets the mark and point
+        ;; after the command runs
+        (save-excursion
+          ;; support selecting from the top down (if) and from the
+          ;; bottom up (unless)
+          (if (> (point) (mark))
+              (comment-or-uncomment-region (mark) (point))
+            (comment-or-uncomment-region (point) (mark))))
+
+      ;; in this case, save-excursion is used to temporarily define a
+      ;; region so that our previous selection (if any) isn't lost
+      (save-excursion
+        (comment-or-uncomment-region
+         (line-beginning-position)
+         (line-end-position))))))
