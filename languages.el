80483852c09fd126cccff0ab4e235b8ec0e38830
Ben Bleything <ben@bleything.net>
2013-05-04 08:23:40 -0700

    Puppetfiles are Ruby

--- languages.el
+++ languages.el
@@ -83,6 +83,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Puppetfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.irbrc$" . ruby-mode))
