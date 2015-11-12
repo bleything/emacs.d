fb5e2ec439fb41157e185391cfa97468d120c2eb
Ben Bleything <ben@bleything.net>
2015-11-12 14:21:59 -0800

    add guide-key

---
+++ elpa/popwin-0.4/popwin-autoloads.el
@@ -0,0 +1,15 @@
+;;; popwin-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("popwin.el") (22085 4443 992956 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; popwin-autoloads.el ends here
