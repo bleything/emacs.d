fce2e12140741077366350ff947cd7337f800008
Ben Bleything <ben@bleything.net>
2016-02-19 11:32:14 -0800

    update a bunch of packages

--- elpa/switch-window-0.9/switch-window-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; switch-window-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("switch-window.el") (21843 35717 721159
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; switch-window-autoloads.el ends here
