9ebfda1da909a89cda4d53d45e9c918c77c7b373
Ben Bleything <ben@bleything.net>
2016-02-19 15:02:36 -0800

    add evil-leader; set leader to ,

--- evil.el
+++ evil.el
@@ -12,4 +12,14 @@
 ;;; evil mode basics
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; evil-leader-mode must be enabled before evil-mode. See the evil-leader docs
+;;; for more details
+(global-evil-leader-mode)
 (evil-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; evil-leader setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; set the leader to ,
+(evil-leader/set-leader ",")
